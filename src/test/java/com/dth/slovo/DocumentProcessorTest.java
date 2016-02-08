e51e78ca3456f919ff6a0ed61d781ccec07607d9
Dth <detharon@gmail.com>
2016-02-08 10:25:37 +0100

    Refactor DocumentProcessorTest
    I've externalized the SimpleEnglish results to a properties file,
    and added a method to read it into a collection.
    
    Also removed the sorting stuff, because it was dependent on the
    WordOccurence compareTo method. Now it works on sets, and they're
    compared by a Hamcrest expression.
    
    Since Hamcrest produces useful error messages, manual listing via
    System.out turned out to be unnecessary as well.

--- src/test/java/com/dth/slovo/DocumentProcessorTest.java
+++ src/test/java/com/dth/slovo/DocumentProcessorTest.java
@@ -2,22 +2,24 @@ package com.dth.slovo;
 
 import com.dth.entity.WordOccurrence;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import org.apache.commons.collections.CollectionUtils;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.BeforeClass;
 
 public class DocumentProcessorTest {
-    private static final String ENGLISH_SIMPLE = "EnglishSimple.txt";
+    private static final String ENGLISH_SIMPLE = "EnglishSimple";
     private static File file = null;
     
     @BeforeClass
     public static void setUp() throws URISyntaxException {
-        file = new File(DocumentProcessorTest.class.getClassLoader().getResource(ENGLISH_SIMPLE).toURI());
+        file = new File(DocumentProcessorTest.class.getClassLoader().getResource(ENGLISH_SIMPLE+".txt").toURI());
     }
     
     @Test
@@ -25,51 +27,27 @@ public class DocumentProcessorTest {
         DocumentProcessor documentProcessor = new DocumentProcessor(file);        
         documentProcessor.processFile();
         
-        ArrayList<WordOccurrence> resultWords = new ArrayList<>(documentProcessor.getWords());        
-        ArrayList<WordOccurrence> expectedWords = getExpectedWords();
+        Set<WordOccurrence> resultWords = new HashSet<>(documentProcessor.getWords());        
+        Set<WordOccurrence> expectedWords = getExpectedWords();
         
-        Collections.sort(resultWords, Collections.reverseOrder());
-        Collections.sort(expectedWords, Collections.reverseOrder());
-        
-        System.out.println("Testing DocumentProcessor on"+ENGLISH_SIMPLE);
-        System.out.println("Expected | Result"+ENGLISH_SIMPLE);
-        
-        for (int i = 0; i < expectedWords.size(); i++) {
-            System.out.println(expectedWords.get(i).toString() + " | " + resultWords.get(i).toString());
-        }
-        
-        assertTrue(CollectionUtils.isEqualCollection(expectedWords, resultWords));
+        assertThat(expectedWords, is(resultWords));
     }
     
-    private ArrayList<WordOccurrence> getExpectedWords() {
-        ArrayList<WordOccurrence> expectedWords = new ArrayList<>();
-        
-        expectedWords.addAll(Arrays.asList(new WordOccurrence[]{
-            new WordOccurrence("i", 4),
-            new WordOccurrence("will", 4),
-            new WordOccurrence("fear", 4),
-            new WordOccurrence("the", 2),
-            new WordOccurrence("where", 1),
-            new WordOccurrence("be", 1),
-            new WordOccurrence("has", 1),
-            new WordOccurrence("gone", 1),
-            new WordOccurrence("there", 1),
-            new WordOccurrence("shall", 1),
-            new WordOccurrence("nothing", 1),
-            new WordOccurrence("only", 1),
-            new WordOccurrence("remain", 1),
-            new WordOccurrence("me", 1),
-            new WordOccurrence("through", 1),
-            new WordOccurrence("pass", 1),
-            new WordOccurrence("it", 1),
-            new WordOccurrence("let", 1),
-            new WordOccurrence("my", 1),
-            new WordOccurrence("face", 1),
-            new WordOccurrence("mind-killer", 1),
-            new WordOccurrence("is", 1),
-            new WordOccurrence("not", 1)
-        }));
-        
+    private Set<WordOccurrence> getExpectedWords() {
+        HashSet<WordOccurrence> expectedWords = new HashSet<>();
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileInputStream(
+                    new File(DocumentProcessorTest.class.getClassLoader()
+                            .getResource(ENGLISH_SIMPLE + ".properties").toURI())));
+        } catch (IOException | URISyntaxException ex) {
+            System.out.println("Could not load+"+ENGLISH_SIMPLE+"+properties file");
+        }
+
+        prop.forEach((k, v) -> {
+            expectedWords.add(new WordOccurrence((String)k, Integer.valueOf((String)v)));
+        });
+
         return expectedWords;
     }
 }
