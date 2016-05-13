f21ef32c715ceb28ffb5a651742055944790fd3c
Dth <detharon@gmail.com>
2016-05-13 13:23:23 +0200

    Move GUI code to FXML file and logic to a corresponding controller
    * GUI controls are defined in an FXML file, with an exception of a
      table columns which need to be created dynamically, so they're
      being set up in the controller.
    
    * The program logic has been moved to a single RootController.
    
    * ProgressBar was set to fill width.

---
+++ src/main/java/com/dth/controller/RootController.java
@@ -0,0 +1,300 @@
+package com.dth.controller;
+
+import com.dth.entity.WordOccurrence;
+import com.dth.service.CsvExportWords;
+import com.dth.service.ExportWords;
+import com.dth.service.WordOccurrenceRepository;
+import com.dth.slovo.DocumentProcessor;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class RootController implements Initializable {
+
+    private final EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("SlovoPU");
+    private Stage stage;
+    
+    @FXML
+    private TableView table;
+    
+    @FXML
+    private ProgressBar progressBar;
+    
+    @FXML
+    private Label status;
+    
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+    
+    public void close() {
+        emfactory.close();
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        createTable();
+        populateTable(1000, false);
+    }
+    
+    // --------------------------------------------------
+    // File menu
+    // --------------------------------------------------
+    
+    @FXML
+    private void openClicked() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Text File");
+        fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        File chosen = fileChooser.showOpenDialog(stage);
+        
+        if (chosen != null) {
+            setBusy();
+            new Thread(() -> {
+                DocumentProcessor documentProcessor = new DocumentProcessor(chosen);
+                documentProcessor.processFile();
+                
+                EntityManager em = emfactory.createEntityManager();
+                em.getTransaction().begin();
+                
+                WordOccurrenceRepository wordRepo = new WordOccurrenceRepository(em);
+                wordRepo.saveWords(documentProcessor.getWords());
+                        
+                em.getTransaction().commit();
+                em.close();
+                
+                Platform.runLater(() -> setReady());
+                populateTable(1000, false);
+            }).start();
+        }
+    }
+    
+    @FXML
+    private void exportClicked() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select file to save");
+        fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        fileChooser.setInitialFileName("words.txt");
+        File chosen = fileChooser.showSaveDialog(stage);
+        
+        if (chosen != null) {
+            setBusy();
+            new Thread(() -> {
+                EntityManager em = emfactory.createEntityManager();
+                em.getTransaction().begin();
+
+                ExportWords export;
+                try {
+                    BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(chosen), "UTF-8"));
+                    
+                    WordOccurrenceRepository wordRepo = new WordOccurrenceRepository(em);
+                    List<WordOccurrence> words = wordRepo.fetchWords(1000);
+                    
+                    export = new CsvExportWords(writer);
+                    export.export(words, 1000);
+                    export.close();
+                } catch (UnsupportedEncodingException | FileNotFoundException ex) {
+                    // TODO handle these exceptions;
+                }
+
+                em.getTransaction().commit();
+                em.close();
+
+                Platform.runLater(() -> setReady());
+            }).start();
+        }
+    }
+
+    @FXML
+    private void exitClicked() {
+        Platform.exit();
+    }
+    
+    // --------------------------------------------------
+    // Data menu
+    // --------------------------------------------------
+    
+    @FXML
+    private void ignoreListClicked() {
+        populateTable(1000, true);
+    }
+    
+    @FXML
+    private void wordListClicked() {
+        populateTable(1000, false);
+    }
+    
+    @FXML
+    private void eraseAllClicked() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setContentText("Are you sure you want to remove all data?"
+                + "\nThis operation is irreversible.");  
+        
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            setBusy();
+            
+            new Thread(() -> {
+                EntityManager em = emfactory.createEntityManager();
+                em.getTransaction().begin();
+
+                WordOccurrenceRepository wordRepo = new WordOccurrenceRepository(em);
+                wordRepo.eraseAllWords();
+
+                em.getTransaction().commit();
+                em.close();
+                
+                Platform.runLater(() -> setReady());
+                populateTable(1000, false);
+            }).start();
+        }
+    }
+    
+    // --------------------------------------------------
+    // Table
+    // --------------------------------------------------
+    
+    @FXML
+    private void tableKeyPressed() {
+        List<WordOccurrence> words = table.getSelectionModel().getSelectedItems();
+        if (words == null) {
+            return;
+        }
+
+        setBusy();
+        new Thread(() -> {
+            for (WordOccurrence w : words) {
+                w.setIgnored(!w.getIgnored());
+            }
+
+            EntityManager em = emfactory.createEntityManager();
+            em.getTransaction().begin();
+
+            WordOccurrenceRepository wordRepo = new WordOccurrenceRepository(em);
+            wordRepo.updateWords(words);
+
+            em.getTransaction().commit();
+            em.close();
+
+            table.getItems().removeAll(words);
+            Platform.runLater(() -> setReady());
+        }).start();
+    }
+    
+    // --------------------------------------------------
+    // General helpers
+    // --------------------------------------------------
+    
+    private void setReady() {
+        status.setText("Status: Ready");
+        progressBar.setProgress(100);
+    }
+
+    private void setBusy() {
+        status.setText("Status: Busy");
+        progressBar.setProgress(-1);
+    }
+
+    private void removeWords() {
+        List<WordOccurrence> words = table.getSelectionModel().getSelectedItems();
+        if (words == null) {
+            return;
+        }
+
+        setBusy();
+        new Thread(() -> {
+            for (WordOccurrence w : words) {
+                w.setIgnored(!w.getIgnored());
+            }
+
+            EntityManager em = emfactory.createEntityManager();
+            em.getTransaction().begin();
+
+            WordOccurrenceRepository wordRepo = new WordOccurrenceRepository(em);
+            wordRepo.updateWords(words);
+
+            em.getTransaction().commit();
+            em.close();
+
+            table.getItems().removeAll(words);
+            Platform.runLater(() -> setReady());
+        }).start();
+    }
+
+    public void populateTable(int rows, boolean ignored) {
+        setBusy();
+        new Thread(() -> {
+            EntityManager em = emfactory.createEntityManager();
+            em.getTransaction().begin();
+
+            WordOccurrenceRepository query = new WordOccurrenceRepository(em);
+            if (!ignored) {
+                table.setItems(FXCollections.observableList(query.fetchWords(rows)));
+            } else {
+                table.setItems(FXCollections.observableList(query.fetchWords(rows, true)));
+            }
+
+            em.getTransaction().commit();
+            em.close();
+
+            Platform.runLater(() -> setReady());
+        }).start();
+    }
+    
+    private void createTable() {
+        table.setPlaceholder(new Label("No words to show, open a new text file."));
+        table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        
+        table.setOnKeyPressed((KeyEvent event) -> {
+            if (event.getCode() == KeyCode.DELETE) {
+                removeWords();
+            }
+        });
+        
+        TableColumn<WordOccurrence, Integer> numCol = new TableColumn("#");
+        numCol.prefWidthProperty().bind(table.widthProperty().divide(6));
+        numCol.setSortable(false);
+        numCol.setCellValueFactory(c
+                -> new ReadOnlyObjectWrapper<>(
+                        table.getItems().indexOf(c.getValue())));
+        
+        TableColumn<WordOccurrence, String> wordCol = new TableColumn("Word");
+        wordCol.prefWidthProperty().bind(table.widthProperty().divide(6).multiply(3));
+        wordCol.setCellValueFactory(new PropertyValueFactory("word"));
+        
+        TableColumn<WordOccurrence, Integer> countCol = new TableColumn("Count");
+        countCol.prefWidthProperty().bind(table.widthProperty().divide(6).multiply(2));
+        countCol.setCellValueFactory(new PropertyValueFactory("count"));
+        
+        table.getColumns().addAll(numCol, wordCol, countCol);
+    }
+}
