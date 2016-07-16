6e7f755d904f8be1bfbb87640b5fc5a21047a2ca
Dth <detharon@gmail.com>
2016-07-16 11:41:13 +0200

    Add icon and title
    * The main stage has been named "Slovo" and it received a
      placeholder icon.

--- src/main/java/com/dth/slovo/Slovo.java
+++ src/main/java/com/dth/slovo/Slovo.java
@@ -4,22 +4,25 @@ import com.dth.controller.RootController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Slovo extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         // Loads the object definitions and populates the Scene graph
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/Root.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
+        stage.setTitle("Slovo");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/icons/icon.png")));
         stage.show();
-        
+
         // Passing stage so that the controller can show dialogs
         RootController controller = loader.getController();
-        controller.setStage(stage);  
-        
+        controller.setStage(stage);
+
         // Database access need to be shut down manually
         stage.setOnCloseRequest(WindowEvent -> {
             controller.close();
